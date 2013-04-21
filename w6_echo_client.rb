9b35e8931b6afbf10df90c1495bc284925e0d7ae
rilian <dmitriyis@gmail.com>
2013-04-21 15:22:14 +0300

    w6 echo server and client

--- w6_echo_client.rb
+++ w6_echo_client.rb
@@ -1,3 +1,8 @@
+require 'socket'
+
+PORT = 2808
+DOMAIN = 'localhost'
+
 ##
 # Client to access EchoServerHandler
 #
@@ -9,6 +14,36 @@
 #  - a "line" at a time, ending with \n or \r
 # 3) output server replies
 #
-class EchoClientHandler
+class EchoClientHandler < TCPSocket
   #
+end
+
+# Validate if can connect to server
+begin
+  clientSession = TCPSocket.new(DOMAIN, PORT)
+rescue
+  puts "Connection to #{DOMAIN}:#{PORT} refused"
+  exit
+end
+
+puts "Starting client connection"
+
+# Array of strings to send to EchoServerHandler
+what_to_say = ['hello1', "hello2\n", "hello3\r"].reverse
+
+# "Say" all given strings, while session is active
+while !(clientSession.closed?) && (message = what_to_say.pop)
+  puts "Sending #{message.inspect}"
+  clientSession.puts message
+
+  puts "Waiting for response"
+  serverMessage = clientSession.gets
+  puts "Received: #{serverMessage}"
+
+  # Close connection when everything is said
+  if what_to_say.empty?
+    puts "Enuff said, closing connection"
+    clientSession.close
+    puts "connection closed"
+  end
 end
--- w6_echo_server.rb
+++ w6_echo_server.rb
@@ -1,3 +1,12 @@
+# Ruby 1.9.3
+# Using TCPServer class to implement Reactor pattern
+# Using plain Ruby to implement Wrapper Facade pattern
+
+require 'socket'
+
+PORT = 2808
+DOMAIN = 'localhost'
+
 ##
 # EchoServerHandler to receive data from EchoClientHandler
 #
@@ -8,6 +17,34 @@
 # 3) output chunk of data (if not limited by \n or \r)
 # 4) output each line, limited by \n or \r
 #
-class EchoServerHandler
+class EchoServerHandler < TCPServer
   #
+end
+
+puts "Starting up server..."
+server = EchoServerHandler.new(DOMAIN, PORT)
+puts "Server #{server} started on #{DOMAIN}:#{PORT}"
+
+while (session = server.accept)
+
+  # Establish a new thread for a EchoClientHandler connection
+  Thread.start do
+
+    # Inform about the new client handler thread
+    thread_id = Thread.current.object_id
+    puts "Initializing thread #{thread_id} for this EchoClientHandler"
+    puts "Received connection from #{session.peeraddr.inspect}"
+
+    # Waiting for client to say something
+    while input = session.gets
+      puts "Received: #{input.inspect}"
+
+      # Send back exactly what was input
+      puts "Echoing back"
+      session.puts "Echo: #{input}"
+    end
+
+    # Nothing to do, quit
+    puts "Thread #{thread_id} exiting"
+  end
 end
