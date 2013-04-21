c36269dfd5fb32181ccdac6f7b6594b0e2ebad9d
rilian <dmitriyis@gmail.com>
2013-04-21 13:47:22 +0300

    server and client stubs

---
+++ w6_echo_client.rb
@@ -0,0 +1,14 @@
+##
+# Client to access EchoServerHandler
+#
+# Functionality
+#
+# 1) connect to EchoServerHandler
+# 2) send some data
+#  - a "chunk" at a time or
+#  - a "line" at a time, ending with \n or \r
+# 3) output server replies
+#
+class EchoClientHandler
+  #
+end
---
+++ w6_echo_server.rb
@@ -0,0 +1,13 @@
+##
+# EchoServerHandler to receive data from EchoClientHandler
+#
+# Functionality
+#
+# 1) accept connection from EchoClientHandler
+# 2) receive message from EchoClientHandler
+# 3) output chunk of data (if not limited by \n or \r)
+# 4) output each line, limited by \n or \r
+#
+class EchoServerHandler
+  #
+end
